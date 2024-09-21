52a8ad51d82fc42042b801126dfa3345ea332793
kareneganyan <kareneganyan1988@mail.ru>
2024-09-21 14:27:30 +0400

    Add html code

---
+++ script.js
@@ -0,0 +1,15 @@
+// dom - document Object model
+// let p = document.getElementById("text")
+// let p = document.getElementsByClassName("p1");
+// let p = document.getElementsByTagName("p");
+// p.innerHTML += "YES"
+// p.innerHTML += "<b> YES </b>"
+// for(var i = 0; i<p.length; i++){
+    // p[i].style.color = "red"
+// p[i].style.marginTop = "50px";
+    // p[i].innerHTML +="YES";
+    // p[i].style["margin-top"] ="50px";
+
+// }
+
+
